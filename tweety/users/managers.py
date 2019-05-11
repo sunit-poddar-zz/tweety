5c6528480782bf48f6a7d01603039206def8ac9d
sunit <sunit@ithaka.travel>
2019-05-12 02:55:27 +0530

    Using django's AbstractBaseUser, created custom manager

---
+++ tweety/users/managers.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    """
+    Custom model manager for User model
+    """
+
+    def create_user(self, first_name, last_name, username, email, password=None):
+
+        if username is None:
+            raise TypeError('Users must have an email address.')
+
+        user = self.model(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, first_name, last_name, username, email, password):
+
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.create_user(first_name, last_name, username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
--- tweety/users/models.py
+++ tweety/users/models.py
@@ -1,16 +1,31 @@
 # django imports
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils.translation import gettext_lazy as _
 
 # project imports
-from tweety.tweety_utils.model_utils import RowInformation
+from tweety_utils.model_utils import RowInformation
+
+# app imports
+from users.managers import UserManager
 
 
-class User(RowInformation):
+class User(RowInformation, AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=140, null=False, blank=False)
     last_name = models.CharField(max_length=140, null=False, blank=False)
-    username = models.CharField(max_length=140, null=False, blank=False, unique=True)
+    username = models.CharField(max_length=140, null=False, blank=False, unique=True, validators=[UnicodeUsernameValidator])
     email = models.CharField(max_length=140, null=False, blank=False, unique=True)
     profile_pic = models.URLField(null=True, blank=True)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
+    objects = UserManager()
 
     def save(self, *args, **kwargs):
         super(User, self).save(*args, **kwargs)
