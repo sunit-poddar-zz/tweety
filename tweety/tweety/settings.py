90f4157c006702fefb67cf2c2ed12b1a170fdadc
sunit <sunit@ithaka.travel>
2019-05-12 01:42:49 +0530

    Replaced sqlite with postgres

--- tweety/tweety/settings.py
+++ tweety/tweety/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'tweety.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable('DATABASE_NAME'),
+        'USER': get_env_variable('DATABASE_USER'),
+        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+        'HOST': get_env_variable('DATABASE_HOST'),
+        'PORT': get_env_variable('DATABASE_PORT'),  # Set to empty string for default.
     }
 }
 
